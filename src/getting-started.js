eb809d94743a7a8d845532907b02c0f346b7ac08
JasonSooter <jason.sooter@gmail.com>
2017-10-24 16:13:46 -0500

    Completed Getting Started Post

---
+++ src/getting-started.js
@@ -0,0 +1,27 @@
+import {
+  forEach,
+  map,
+  filter,
+  reject,
+  find,
+  reduce
+} from 'ramda';
+const double = x => x * 2;
+const isEven = x => x % 2 === 0;
+const add = (accum, value) => accum + value;
+
+const myArray = [
+  'Jason',
+  'Andrea',
+  'Ellie',
+  'Mac',
+  'Poppy'
+];
+
+forEach(value => console.log(value), myArray);
+console.log(map(double, [1, 2, 3]));
+console.log(filter(isEven, [1, 2, 3, 4]));
+console.log(reject(isEven, [1, 2, 3, 4]));
+console.log(find(isEven, [1, 2, 3, 4]));
+
+console.log(reduce(add, 5, [1, 2, 3, 4]));
